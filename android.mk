845aad2ec93ceaf314e45c652fdd76772e725449
andreibioazum-cell <andreibioazum@gmail.com>
2026-06-27 15:40:54 +0400

    Update android.mk

--- android.mk
+++ android.mk
@@ -3,6 +3,6 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := main
 LOCAL_SRC_FILES := src/main.c src/game.c src/world.c src/math.c $(NDK_ROOT)/sources/android/native_app_glue/android_native_app_glue.c
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/src $(NDK_ROOT)/sources/android/native_app_glue
-LOCAL_LDLIBS := -landroid -llog -lEGL -lGLESv2 -lm
+LOCAL_LDLIBS := -landroid -llog -lEGL -lGLES2v -lm
 LOCAL_CFLAGS := -DANDROID -std=c99 -O2
 include $(BUILD_SHARED_LIBRARY)
